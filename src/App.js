27d72ec8c32c7aeb4429b1af4a55fdd07fbbb4d6
John Climie <johnclimie00@gmail.com>
2022-03-29 14:21:02 -0400

    Created resume route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Footer from "../src/components/Footer";
 import Portfolio from "../src/components/Portfolio";
 import Content from "../src/components/AboutMe";
 import Contact from "../src/components/Contact";
+import Resume from "../src/components/Resume";
 
 function App() {
     return (
@@ -20,9 +21,7 @@ function App() {
                <Route path="/" element={<Content />} />
                <Route path="/portfolio" element={<Portfolio />} />
                <Route path="/contact" element={<Contact />} />
-               {/* <Route Resume="/resume" element={} /> */}
-
-               {/* </Route> */}
+               <Route path="/resume" element={<Resume />} />
             </Routes>
         <Footer />
         </BrowserRouter>
---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Style.css';
+
+function Resume() {
+    return (
+        <section id="resume">
+            <h1>You can view my resume <a href="#"><span>here</span></a></h1>
+        </section>
+
+    )
+}
+
+export default Resume;

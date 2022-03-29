0c11402bac2656a82528c99916ce27c96b7fd897
John Climie <johnclimie00@gmail.com>
2022-03-29 17:11:18 -0400

    Filled out projects file

--- src/projects.js
+++ src/projects.js
@@ -1,51 +1,51 @@
 const projects = [
     {
-        name: "Test 1",
-        img: "",
-        description: "",
-        languages: "",
-        deployment: "",
-        github: ""
+        name: "Text Editor",
+        img: "https://github.com/johnclimie/TextEditor/raw/master/screenshot.png",
+        description: "A PWA full-stack web applicaiton, where a user can enter text and the user input would save. The user can return to the application anytime.",
+        languages: "JavaScript, CSS, HTML",
+        deployment: "https://text-editor-jjc.herokuapp.com/",
+        github: "https://github.com/johnclimie/TextEditor"
     },
     {
-        name: "Test 2",
-        img: "",
-        description: "",
-        languages: "",
-        deployment: "",
-        github: ""
+        name: "Tech Blog",
+        img: "https://github.com/johnclimie/TechBlog/raw/master/screenshot.png",
+        description: "A full-stack web application, where a user can create an account and make posts relating to technology. The user can also comment on other's posts.",
+        languages: "JavaScript, Handlebars",
+        deployment: "https://techblogjc.herokuapp.com/",
+        github: "https://github.com/johnclimie/TechBlog"
     },
     {
-        name: "Test 3",
-        img: "",
-        description: "",
-        languages: "",
-        deployment: "",
-        github: ""
+        name: "Weather Dashboard",
+        img: "https://github.com/johnclimie/WeatherDashboard/raw/master/assets/screenshot.png",
+        description: "A front-end webpage, where a user can input a locaiton and view the weather for that location, along with a 5 day forecast.",
+        languages: "HTML, Bootstrap, JavaScript",
+        deployment: "https://johnclimie.github.io/WeatherDashboard/",
+        github: "https://github.com/johnclimie/WeatherDashboard"
     },
     {
-        name: "Test 4",
-        img: "",
-        description: "",
-        languages: "",
-        deployment: "",
-        github: ""
+        name: "Random Password Generator",
+        img: "https://github.com/johnclimie/RandomPasswordGenerator/raw/master/screenshot.png",
+        description: "A front-end webpage, where a user can select a critera for a password, and a random password will be generated based on the critera.",
+        languages: "HTML, CSS, JavaScript",
+        deployment: "https://johnclimie.github.io/RandomPasswordGenerator/",
+        github: "https://github.com/johnclimie/RandomPasswordGenerator"
     },
     {
-        name: "Test 5",
-        img: "",
-        description: "",
-        languages: "",
-        deployment: "",
-        github: ""
+        name: "Code Quiz",
+        img: "https://github.com/johnclimie/CodeQuiz/raw/master/screenshot.png",
+        description: "A front-end webpage, where a user can partake in a quiz about JavaScript fundamentals. The user's high score can be stored.",
+        languages: "JavaScript, CSS, HTML",
+        deployment: "https://johnclimie.github.io/CodeQuiz/",
+        github: "https://github.com/johnclimie/CodeQuiz"
     },
     {
-        name: "test 6",
-        img: "",
-        description: "",
-        languages: "",
-        deployment: "",
-        github: ""
+        name: "Subscription Tracker",
+        img: "https://github.com/ramksharma1/project-two-subscription-tracker/raw/main/Public/assets/images/login-image_mobileresponsive.jpg",
+        description: "A full-stack web application, where a user can create an account and keep track of online subscriptions. This is useful to keep track of where your money goes.",
+        languages: "JavaScript, Handlebars, CSS",
+        deployment: "https://subscriptiontracker1.herokuapp.com/",
+        github: "https://github.com/ramksharma1/project-two-subscription-tracker"
     }
 ]
 
